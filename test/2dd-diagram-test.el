715e4872350c3b8f40c3ed8f6d5ba07c2872e747
pallagun <pallagun@no.email>
2020-03-06 05:48:29 -0500

    correcting a unit test for new implementation

--- test/2dd-diagram-test.el
+++ test/2dd-diagram-test.el
@@ -1,6 +1,15 @@
 (require 'ert)
 (require '2dd-diagram)
 
+(defun 2dd--lists-equal (a b)
+  (if (eq (length a) (length b))
+      (cl-loop for a-elt in a
+               for b-elt in b
+               when (not (eq a-elt b-elt))
+               return nil
+               finally return t)
+    nil))
+
 (ert-deftest 2dd-diagram-test-001 ()
   "Simple diagram, one shape"
   (let ((diagram (2dd-diagram :canvas (2dd-canvas- 0 10 0 10)
@@ -67,48 +76,48 @@
       (should (equal output expected)))
 
     ;; nothing is in the bottom left of the bottom left pixel
-    (should (eq (2dd-find-drawing-selection diagram
-                                            (2dg-rect :x-min 0.0
-                                                      :x-max 0.5
-                                                      :y-min 0.0
-                                                      :y-max 0.5)
-                                            child-fn)
-                nil))
+    (should (2dd--lists-equal (2dd-find-drawing-selection diagram
+                                                          (2dg-rect :x-min 0.0
+                                                                    :x-max 0.5
+                                                                    :y-min 0.0
+                                                                    :y-max 0.5)
+                                                          child-fn)
+                              nil))
     ;; in the top left of the parent box should be only the parent.
-    (should (eq (2dd-find-drawing-selection diagram
-                                            (2dg-rect :x-min 3.0
-                                                      :x-max 4.5
-                                                      :y-min 7.0
-                                                      :y-max 8.5)
-                                            child-fn)
-                parent))
+    (should (2dd--lists-equal (2dd-find-drawing-selection diagram
+                                                          (2dg-rect :x-min 3.0
+                                                                    :x-max 4.5
+                                                                    :y-min 7.0
+                                                                    :y-max 8.5)
+                                                          child-fn)
+                              (list parent)))
 
     ;; in the bottom left is child A
-    (should (eq (2dd-find-drawing-selection diagram
-                                            (2dg-rect :x-min 2.0
-                                                      :x-max 3.0
-                                                      :y-min 3.0
-                                                      :y-max 4.0)
-                                            child-fn)
-                child-a))
+    (should (2dd--lists-equal (2dd-find-drawing-selection diagram
+                                                          (2dg-rect :x-min 2.0
+                                                                    :x-max 3.0
+                                                                    :y-min 3.0
+                                                                    :y-max 4.0)
+                                                          child-fn)
+                              (list child-a parent)))
 
     ;; in the top right is child B
-    (should (eq (2dd-find-drawing-selection diagram
-                                            (2dg-rect :x-min 5.0
-                                                      :x-max 6.5
-                                                      :y-min 5.0
-                                                      :y-max 6.5)
-                                            child-fn)
-                child-b))
+    (should (2dd--lists-equal (2dd-find-drawing-selection diagram
+                                                          (2dg-rect :x-min 5.0
+                                                                    :x-max 6.5
+                                                                    :y-min 5.0
+                                                                    :y-max 6.5)
+                                                          child-fn)
+                              (list child-b parent)))
 
     ;; if you select everything it should return the first and deepest
     ;; leaf node drawing
-    (should (eq (2dd-find-drawing-selection diagram
-                                            (2dg-rect :x-min 0.5
-                                                      :x-max 9.0
-                                                      :y-min 0.5
-                                                      :y-max 10.5)
-                                            child-fn)
-                child-a))))
+    (should (2dd--lists-equal (2dd-find-drawing-selection diagram
+                                                          (2dg-rect :x-min 0.5
+                                                                    :x-max 9.0
+                                                                    :y-min 0.5
+                                                                    :y-max 10.5)
+                                                          child-fn)
+                              (list child-a parent)))))
 
 (provide '2dd-diagram-test)
